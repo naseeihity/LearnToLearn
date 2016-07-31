10e33b97004a9f743e3603553ed0bbc0492773c0
Coacooo <naseeihility@outlook.com>
2016-07-31 11:28:25 +0800

    #

--- kails/assets/javascripts/application.js
+++
@@ -1,4 +0,0 @@
-require('bootstrap/dist/css/bootstrap.css');
-require('bootstrap/dist/js/bootstrap');
-require('../stylesheets/base.css');
-require('../stylesheets/movie.css');
--- kails/routes/home.js
+++
@@ -1,8 +0,0 @@
-import Router from 'koa-router'
-import home from '../controllers/home'
-
-const router = Router()
-router.get('/', home.index)
-router.get('about', home.about)
-router.get('movie', home.movie)
-export default router
