85669c4ef82ca377f75a843efc2e1b6fea994d85
Coacooo <naseeihility@outlook.com>
2016-08-21 16:25:25 +0800

    update the content

--- lazyLoadingReact/assets/javascripts/components/ConstValue.js
+++ lazyLoadingReact/assets/javascripts/components/ConstValue.js
@@ -1,8 +1,8 @@
 export const IMG_DETAIL = [
-	{ src: 'http://7xr09v.com1.z0.glb.clouddn.com/lion-wild-africa-african.jpg',
-	  alt: 'tiger'},
-	{ src: 'http://7xr09v.com1.z0.glb.clouddn.com/pexels-photo-138905.jpeg',
+	{ src: 'http://7xr09v.com1.z0.glb.clouddn.com/shadow.jpg',
+	  alt: 'shadow'},
+	{ src: 'http://7xr09v.com1.z0.glb.clouddn.com/mountain.jpg',
 	  alt: 'mountain'},
-	{ src: 'http://7xr09v.com1.z0.glb.clouddn.com/pexels-photo-130111.jpeg',
-	  alt: 'family'}
+	{ src: 'http://7xr09v.com1.z0.glb.clouddn.com/girl.jpeg',
+	  alt: 'girl'}
 ];
